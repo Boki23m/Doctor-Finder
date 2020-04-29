358346077abb6e28a86a38491e8446cf685e2820
Boki23m <boki23usa@yahoo.com>
2020-04-29 14:19:35 -0700

    API updated

---
+++ javascript/config.js
@@ -0,0 +1,5 @@
+var config = {
+    BETTERDR_API_KEY : "9dd880db4c3a037bcf6b4b352db255c2",
+    NEWS_API_KEY : '7fabcb6f2bdc479e826d08269c5e8647',
+    GGOGLE_API_KEY : 'AIzaSyDuQGBn62T6SSvtH85jK6jh0ypKiqx8aYk'
+  }
